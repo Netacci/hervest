4d62e38e87586c928e858848a5b4bfec481e3734
Chineta Adinnu <52791481+Netacci@users.noreply.github.com>
2022-03-06 12:27:32 +0100

    route added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import Dashboard from './components/Dashboard';
+import { Routes, Route } from 'react-router-dom';
 
 const App = () => {
-  return <Dashboard />;
+  return (
+    <Routes>
+      <Route path='/' element={<Dashboard />} />
+    </Routes>
+  );
 };
 
 export default App;
